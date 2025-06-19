34e612479f71baf6311b6cc8f58296f254a2f6b5
martinyocupicio228307 <martin.yocupicio228307@potros.itson.edu.mx>
2025-06-19 09:43:26 -0700

    Revert "Pequeñas modificaciones y el task_view se ha agregado"
    This reverts commit f8c83403338d17a0df2829dbef83a93f16272511.

--- app/src/main/java/com/example/mydigimind_yocupiciomartin/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/mydigimind_yocupiciomartin/ui/dashboard/DashboardFragment.kt
@@ -1,20 +1,15 @@
 package com.example.mydigimind_yocupiciomartin.ui.dashboard
 
-import android.app.TimePickerDialog
-import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
-import android.widget.TimePicker
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.mydigimind_yocupiciomartin.R
 import com.example.mydigimind_yocupiciomartin.databinding.FragmentDashboardBinding
 import com.example.mydigimind_yocupiciomartin.ui.home.HomeViewModel
-import java.text.SimpleDateFormat
 
 class DashboardFragment : Fragment() {
 
@@ -27,21 +22,9 @@ class DashboardFragment : Fragment() {
     ): View? {
         dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+        dashboardViewModel.text.observe(viewLifecycleOwner, {
 
-        val btn_time: Button = root.findViewById(R.id.btn_time)
-
-        btn_time.setOnClickListener {
-            val cal = Calendar.getInstance()
-            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
-                cal.set(Calendar.HOUR_OF_DAY, hour)
-                cal.set(Calendar.MINUTE, minute)
-
-                btn_time.text = SimpleDateFormat("HH:mm").format(cal.time)
-            }
-            TimePickerDialog(root.context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY),
-                cal.get(Calendar.MINUTE), true).show()
-        }
-
+        })
         return root
     }
 }
